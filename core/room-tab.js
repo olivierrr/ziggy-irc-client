cdbb817a1bda23f0d2236a410fa389868e1d4605
olivierrr <oliviersinbox@gmail.com>
2014-08-26 07:22:09 -0700

    room-tab form

--- core/room-tab.js
+++ core/room-tab.js
@@ -1,16 +1,28 @@
 var Handlebars = require('handlebars')
 
+/*
+	room ui plugin
+
+	todo:
+	-rewrite
+*/
+
 module.exports = function(tab) {
 
 	var document = this.ziggy.dom
-	//var tab = this.getById(id)
+	var ziggy = this.ziggy
 	tab.messages = []
 
-	var template = Handlebars.compile(document.getElementById('tab_room_template').innerHTML)
-	var context = {}
+	var form_template = Handlebars.compile(document.getElementById('tab_room_template_q').innerHTML)
+	var room_template = Handlebars.compile(document.getElementById('tab_room_template').innerHTML)
+
+	var chatbox
 
 	this.ee.on('focus#'+tab.id, function() {
-		document.getElementById('TAB').innerHTML = template(context)
+
+		document.getElementById('TAB').innerHTML = form_template()
+
+		onClick('#roomSubmit', roomSubmit)
 	})
 
 	this.ee.on('blur#'+tab.id, function() {
@@ -24,9 +36,60 @@ module.exports = function(tab) {
 		}
 	}
 
+	// should validate, handle missing fields/errors
+	function roomSubmit(e) {
 
+		var nick = document.getElementById('formNick').value || 'ziggyClient'
+		var server = document.getElementById('formServer').value || 'irc.freenode.net'
+		var channel = document.getElementById('formChannel').value || '#testingbot'
+		
+		joinRoom(nick, server, channel)
+	}
+
+	function joinRoom(nick, server, channel) {
+
+		document.getElementById('TAB').innerHTML = room_template()
+		chatbox = document.getElementById('TAB_ROOM')
+
+		ziggy.joinChannel('irc.freenode.net', '#testingbot', 'ziggy-client')
+
+		.on('message', function(user, channel, text) {
+			console.log('message')
+			tab.messages.push(text)
+			appendToChat(user.nick, text)
+		})
+
+		.on('pm', function(user, text) {
+			console.log('pm')
+		})
+
+		.on('nick', function(oldNick, user, channels) {
+			console.log('nick')
+		})
+
+		.on('join', function(channel, user) {
+			console.log('join')
+		})
+
+		.on('part', function(user, channel, reason) {
+			console.log('part')
+		})
+
+		.on('quit', function(user, reason) {
+			console.log('quit')
+		})
+
+		.on('kick', function(kicked, kickedBy, channel, reason) {
+			console.log('kick')
+		})
+
+		.on('topic', function(channel, topic, nick) {
+			console.log('topic')
+		})
+	}
+
+	// temp
 	function appendToChat(from, msg){
-		var chatbox = document.getElementById('TAB_ROOM')
 		// create and append
 		var o = document.createElement('div')
 		o.className = 'message'
@@ -38,40 +101,6 @@ module.exports = function(tab) {
 	}
 
 
-	this.ziggy.joinChannel('irc.freenode.net', '#testingbot', 'ziggy-client')
-
-	.on('message', function(user, channel, text) {
-		console.log('message')
-		tab.messages.push(text)
-		appendToChat(user.nick, text)
-	})
-
-	.on('pm', function(user, text) {
-		console.log('pm')
-	})
-
-	.on('nick', function(oldNick, user, channels) {
-		console.log('nick')
-	})
-
-	.on('join', function(channel, user) {
-		console.log('join')
-	})
-
-	.on('part', function(user, channel, reason) {
-		console.log('part')
-	})
-
-	.on('quit', function(user, reason) {
-		console.log('quit')
-	})
-
-	.on('kick', function(kicked, kickedBy, channel, reason) {
-		console.log('kick')
-	})
-
-	.on('topic', function(channel, topic, nick) {
-		console.log('topic')
-	})
+	
 
 }
